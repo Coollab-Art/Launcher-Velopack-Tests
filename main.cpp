78ef67b65cf5b8789d450a4b4150c34852017bf4
AnassDoublal <a.doublal02@gmail.com>
2025-06-20 01:56:04 +0200

    Testing GitHub Actions workflow

--- main.cpp
+++ main.cpp
@@ -1,61 +1,66 @@
-#include "velopack/include/Velopack.hpp"
-#include <cstddef>
-#include <iostream>
+#include <codecvt>
 #include <fstream>
+#include <iostream>
+#include <locale>
+#include "velopack/include/Velopack.hpp"
 
 static void update_app()
 {
-    try{
+    try
+    {
         Velopack::UpdateManager manager("https://coollab-art.github.io/Launcher-Velopack-Tests");
         std::wcout << L"Checking for updates..." << std::endl;
 
         auto updInfo = manager.CheckForUpdates();
-        if (!updInfo.has_value()) {
+        if (!updInfo.has_value())
+        {
             std::wcout << L"No updates found." << std::endl;
-            return; // no updates available
+            return;
         }
 
         std::wcout << L"Update found: " << updInfo->TargetFullRelease.Version.c_str() << std::endl;
 
-        // download the update, optionally providing progress callbacks
         manager.DownloadUpdates(updInfo.value());
         std::wcout << L"Downloaded update." << std::endl;
 
-        // prepare the Updater in a new process, and wait 60 seconds for this process to exit
         manager.WaitExitThenApplyUpdates(updInfo.value());
         std::wcout << L"Applying update..." << std::endl;
 
-        exit(0); // exit the app to apply the update
-    } 
-    catch (const std::exception &ex) {
+        exit(0); // Terminate so Velopack can restart
+    }
+    catch (const std::exception& ex)
+    {
         std::cerr << "[EXCEPTION] " << ex.what() << "\n";
-    } 
-    catch (...) {
+    }
+    catch (...)
+    {
         std::cerr << "[EXCEPTION] Unknown exception\n";
     }
 }
 
-void LoggerCallback(void* user_data, const char* level, const char* message) {
+void LoggerCallback(void* /*user_data*/, const char* level, const char* message)
+{
     std::ofstream log("velopack_log.txt", std::ios::app);
-    log << "[" << level << "] " << message << std::endl;
+    if (log.is_open())
+        log << "[" << level << "] " << message << std::endl;
 }
 
-int wmain(int argc, wchar_t *argv[], wchar_t *envp[])
+int wmain(int argc, wchar_t* argv[], wchar_t* /*envp*/[])
 {
-    // This should run as early as possible in the main method.
-    // Velopack may exit / restart the app at this point. 
-    // See VelopackApp class for more options/configuration.
-    Velopack::VelopackApp::Build().SetLogger(LoggerCallback, nullptr).Run();
+    Velopack::VelopackApp::Build()
+        .SetLogger(LoggerCallback, nullptr)
+        .Run();
 
     std::wcout << L"App launched with arguments: ";
-    for (int i = 0; i < argc; ++i) {
+    for (int i = 0; i < argc; ++i)
+    {
         std::wcout << argv[i] << L" ";
     }
     std::wcout << std::endl;
 
-    // ... your other startup code here
-    update_app();
-    std::wcout << "Running version: 1.0.8\n" << std::endl;
+    std::wcout << L"Running version: 1.0.9\n"
+               << std::endl;
 
+    update_app();
     return 0;
 }
